69d5daf7b48b700ec2cfbe5428478a3e43ef204e
Bob Smith <bob@test.com>
2023-07-03 16:02:34 -0400

    part 2 done

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+const port = 3000;
+//app.use((req, res, next) => {
+//  console.log('Time:', Date.now())
+//  next()
+//})
 
-app.use((req, res, next) => {
-  console.log('Time:', Date.now())
-  next()
-})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
 app.get('/api/planets/', (req, res, next) => {
     res.send('planet data example');
   })
+
+  
